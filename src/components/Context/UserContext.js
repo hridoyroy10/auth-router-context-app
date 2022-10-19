0daa4b4b0f256050f4b82cf346fccf592848cb55
hridoy10 <hridoyroyy.info@gmail.com>
2022-10-19 21:28:19 +0600

    first commit

---
+++ src/components/Context/UserContext.js
@@ -0,0 +1,19 @@
+import React, { createContext } from 'react';
+
+const AuthContext = createContext(); 
+
+    
+
+const UserContext = ({children}) => {
+    const user ={displayName: 'Hridoy Roy'}
+
+    const authInfo = {user: user}
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default UserContext;
