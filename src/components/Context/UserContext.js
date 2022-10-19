7bd899dd81cf5dfe24643558ab3cd17288984e9f
hridoy10 <hridoyroyy.info@gmail.com>
2022-10-20 00:04:50 +0600

    UserContext added

--- src/components/Context/UserContext.js
+++ src/components/Context/UserContext.js
@@ -1,16 +1,23 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import {getAuth} from 'firebase/auth';
+import app from './../../firebase/firebase.init';
 
-const AuthContext = createContext(); 
+export const AuthContext = createContext()
 
-    
+const auth = getAuth(app)  
 
 const UserContext = ({children}) => {
-    const user ={displayName: 'Hridoy Roy'}
+
+    const [ user, setUser] = useState({displayName: 'Hridoy Roy'})
+    // const user ={displayName: 'Hridoy Roy'}
 
     const authInfo = {user: user}
 
     return (
-        <AuthContext.Provider value={authInfo}>
+        // <AuthContext.Provider value={authInfo}>
+        //     {children}
+        // </AuthContext.Provider>
+        <AuthContext.Provider value ={authInfo}>
             {children}
         </AuthContext.Provider>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-
+import { AuthContext } from './Context/UserContext';
 const Header = () => {
+    const {user} = useContext(AuthContext);
+    console.log('context', user);
     return (
         <div>
             <div className="navbar bg-primary text-primary-content">
@@ -9,7 +11,9 @@ const Header = () => {
                 <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
                 <Link className="btn btn-ghost normal-case text-xl" to='/login'>Log In</Link>
                 <Link className="btn btn-ghost normal-case text-xl" to='/register'>Register</Link>
+                {user?.displayName && <span>Welcome, {user.displayName}</span>}
             </div>
+  
         </div>
     );
 };
